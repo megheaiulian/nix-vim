00f7615bc1f4cc5b26e065bdea6d3cde6dd3b489
Meghea Iulian <iulian.meghea@gmail.com>
2022-10-31 07:23:32 +0200

    feat: adjust config

--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
 
   outputs = { self, nixpkgs }:
     let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       call = f: args: f (builtins.intersectAttrs (builtins.functionArgs f) args);
       lib = import ./lib.nix;
       config = import ./config.nix;
+      mkPackage = pkgs: call lib.mkNeovim pkgs (call config pkgs);
     in
     {
       inherit config lib call;
-      defaultPackage."x86_64-linux" =
-        let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        in call lib.mkNeovim pkgs (call config pkgs);
+      defaultPackage = forAllSystems (system: mkPackage (import nixpkgs { inherit system; }));
     };
 }
