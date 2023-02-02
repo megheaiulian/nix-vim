31ef6299232a7e154810823b936e1b561a775f0d
Meghea Iulian <iulian.meghea@gmail.com>
2023-02-02 08:18:03 +0200

    feat: add toml format

--- config.nix
+++ config.nix
@@ -249,6 +249,7 @@ let
             null_ls.builtins.completion.spell,
             null_ls.builtins.formatting.nixpkgs_fmt,
             null_ls.builtins.formatting.prettier
+            null_ls.builtins.formatting.taplo
           },
         })
         EOF
